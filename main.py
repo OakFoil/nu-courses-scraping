b45b5b7f319acebed509b89224f33d100c7aac51
OakFoil <elsennahway@gmail.com>
2026-06-09 16:39:25 +0300

    add automated cookie retrivial

---
+++ main.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import sync_playwright
+import json
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+
+    page = browser.new_page()
+    page.goto("https://register.nu.edu.eg/PowerCampusSelfService/Home/LogIn", wait_until="networkidle")
+
+    page.fill("#txtUsername", "${USERNAME}")
+    page.click("#btnNext")
+    page.wait_for_load_state("networkidle")
+    if (page.url != "https://register.nu.edu.eg/PowerCampusSelfService"):
+      page.fill("#txtPassword", "${PASSWORD}")
+      page.click("#btnSignIn")
+    page.wait_for_selector("#mainContent")
+
+    cookies = page.context.cookies("https://register.nu.edu.eg")
+    cookie_header = "; ".join([f"{cookie['name']}={cookie['value']}" for cookie in cookies]) # pyright: ignore[reportTypedDictNotRequiredAccess]
+
+    json = json.dumps({"COOKIE": cookie_header})
+
+    print(json)
+
+    browser.close()
