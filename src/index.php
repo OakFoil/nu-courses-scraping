7d2c2734b9b757f1e3dfd80dd73e1575415a9f68
OakFoil <elsennahway@gmail.com>
2026-07-20 07:09:53 +0300

    change cors header

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: https://sched-gen.rf.gd");
     header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Expose-Headers: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("Access-Control-Expose-Headers: Content-Type");
     header("Content-Type: application/json");
 
     $curl = curl_init();
